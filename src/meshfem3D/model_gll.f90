cf8b6b6bae9cb86fbe40c23c3f5c51cecfc729bf
Daniel Peter <daniel.b.peter@gmail.com>
2024-10-23 15:11:24 +0200

    comments out `sequence` keyword (not needed) for gll model variables

--- src/meshfem3D/model_gll.f90
+++ src/meshfem3D/model_gll.f90
@@ -39,7 +39,15 @@
 
   ! GLL model_variables
   type model_gll_variables
-    sequence
+    !TODO: check if `sequence` is needed
+    !
+    !      in principle, `sequence` is only needed to explicity map the memory structure, for example when
+    !      the variable like `MGLL_V` gets passed as a routine argument, making sure it gets accessed correctly.
+    !      however, here we don't pass the objects MGLL_V, MGLL_V_IC to routines, but only use the objects to store arrays
+    !      within this module.
+    !
+    !sequence
+
     ! tomographic iteration model on GLL points
     real(kind=CUSTOM_REAL) :: scale_velocity,scale_density,scale_GPa
 
@@ -54,7 +62,9 @@
 
     ! number of elements (crust/mantle elements)
     integer :: nspec
-    integer :: dummy_pad ! padding 4 bytes to align the structure
+
+    ! in case we use `sequence` to align memory addresses
+    !integer :: dummy_pad ! padding 4 bytes to align the structure
   end type model_gll_variables
 
   ! crust/mantle model
