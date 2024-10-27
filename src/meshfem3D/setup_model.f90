88b9a2de694fd143268e77a41785e07877d8a4bc
Daniel Peter <daniel.b.peter@gmail.com>
2024-10-27 15:36:52 +0100

    updates user output

--- src/meshfem3D/setup_model.f90
+++ src/meshfem3D/setup_model.f90
@@ -130,7 +130,8 @@
 
   subroutine sm_output_info()
 
-  use constants, only: IMAIN,NGLLX,NGLLY,NGLLZ,NGNOD,NGNOD2D,N_SLS,ASSUME_PERFECT_SPHERE
+  use constants, only: IMAIN,NGLLX,NGLLY,NGLLZ,NGNOD,NGNOD2D,N_SLS,ASSUME_PERFECT_SPHERE, &
+    USE_OLD_VERSION_FORMAT
   use shared_parameters, only: R_PLANET_KM
 
   use meshfem_models_par
@@ -163,6 +164,12 @@
   write(IMAIN,*) 'Shape functions defined by NGNOD = ',NGNOD,' control nodes'
   write(IMAIN,*) 'Surface shape functions defined by NGNOD2D = ',NGNOD2D,' control nodes'
   write(IMAIN,*)
+
+  ! backward compatibility
+  if (USE_OLD_VERSION_FORMAT) then
+    write(IMAIN,*) 'using old version backward compatibility (versions 7.0 to 8.0)'
+    write(IMAIN,*)
+  endif
 
   ! model user parameters
   write(IMAIN,*) 'model: ',trim(MODEL)
